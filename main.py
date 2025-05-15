4209fe7d5cceab35477880c9b6bdbd4a5868684c
davelopeer <davidbarenco@gmail.com>
2025-05-15 13:27:13 -0300

    Criação do endpoint de health

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import datetime
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+startedAt = datetime.datetime.now()
 
 @app.get('/')
 def index():
@@ -9,3 +13,12 @@ def index():
     age = os.environ['AGE']
 
     return {'Olá': f'Eu sou o {name} e tenho {age} anos.'}
+
+@app.get('/health')
+def health_check():
+    duration = datetime.datetime.now() - startedAt
+
+    if duration.seconds > 25:
+        return Response(status_code=500)
+    else:
+        return JSONResponse(status_code=200, content={'status': 'ok'})
